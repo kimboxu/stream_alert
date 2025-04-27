5aae06a2d459ccb7d736ee9c9a073060c462cbc7
kimboxu <kimboxu@gmail.com>
2025-04-27 21:21:53 +0900

    불필요한 코드 삭제

--- lib/py/discordbot.py
+++ lib/py/discordbot.py
@@ -100,7 +100,8 @@ async def generic_chat(init: initVar, platform_name: str, message_class):
 
 async def main():
     state = StateManager.get_instance()
-    init = await state.initialize()
+    init = state.get_init()
+    if init is None: init = await state.initialize()
     from my_app import initialize_firebase
     initialize_firebase(False)
 
