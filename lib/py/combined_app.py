6eb47ae07094595e25f5e39a0c65a2a287eacc65
kimboxu <kimboxu@gmail.com>
2025-05-19 22:43:37 +0900

    change port

--- lib/py/combined_app.py
+++ lib/py/combined_app.py
@@ -175,7 +175,7 @@ def main():
     
     # Flask 앱 설정 및 실행
     app = setup_flask_app()
-    app.run(host="0.0.0.0", port=443, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
 if __name__ == "__main__":
     main()
