dc8eb17a4d2ab59fa56a360a4d174f54ce3b2de0
kimboxu <kimboxu@gmail.com>
2025-05-21 18:53:42 +0900

    save_user_settings debug

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -17,16 +17,8 @@ import '../utils/cache_helper.dart';
 import '../utils/url_helper.dart';
 
 class ApiService {
-  // static const String baseUrl = 'http://192.168.0.4:5000'; // 디버깅 용도
+  // static const String baseUrl = 'http://192.168.0.8:5000'; // 디버깅 용도
   static const String baseUrl = 'https://discordbotsetting.duckdns.org'; // 오라클 서버 주소
-  // static String get baseUrl {
-  //   if (kIsWeb) {
-  //     // CORS 프록시 서비스 사용 (테스트용)
-  //     return 'https://cors-anywhere.herokuapp.com/http://146.56.98.203:5000';
-  //   } else {
-  //     return 'http://146.56.98.203:5000';
-  //   }
-  // }
 
   // 이미지 URL을 프록시 URL로 변환하는 함수
   static processImageUrl(String url) {
