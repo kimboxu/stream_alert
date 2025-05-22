5bb2f59138272fd7f2b26677541fe3f1ad50d891
kimboxu <kimboxu@gmail.com>
2025-05-22 22:13:56 +0900

    add env

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// main.dart
-
 // ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, unnecessary_import
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
