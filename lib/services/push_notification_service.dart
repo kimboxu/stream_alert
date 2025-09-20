b9308932e4c3e5d604d2f00dcad2f555d43bbca5
kimboxu <kimboxu@gmail.com>
2025-09-20 15:44:11 +0900

    _firebaseMessagingBackgroundHandler

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -208,11 +208,11 @@ class PushNotificationService {
       }
 
       // 백그라운드 핸들러 설정
-      // if (!kIsWeb) {
-      //   FirebaseMessaging.onBackgroundMessage(
-      //     _firebaseMessagingBackgroundHandler,
-      //   );
-      // }
+      if (!kIsWeb) {
+        FirebaseMessaging.onBackgroundMessage(
+          _firebaseMessagingBackgroundHandler,
+        );
+      }
       // 안드로이드용 로컬 알림 설정
       const AndroidInitializationSettings androidSettings =
           AndroidInitializationSettings('@mipmap/launcher_icon');
