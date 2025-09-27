79b28815d2b95395123c0195506c12da0d09ee5e
kimboxu <kimboxu@gmail.com>
2025-09-28 00:05:37 +0900

    debug _send_notification

--- lib/py/chat_analyzer.py
+++ lib/py/chat_analyzer.py
@@ -874,7 +874,7 @@ class ChatAnalyzer:
             if self.init.DO_TEST: 
                 image_url = 'https://i.imgur.com/Mwbjz5a.jpeg'
             else:
-                image_url = upload_image_to_imgbb(self.channel_id, thumbnail_url, platform_prefix = platform_name)
+                image_url = await upload_image_to_imgbb(self.channel_id, thumbnail_url, platform_prefix = platform_name)
             
  
             timeline_comments = await self._make_highlight_chat([highlight])
