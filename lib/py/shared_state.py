32b8b35069eace08601ba02ad12d46dcba33b790
kimboxu <kimboxu@gmail.com>
2025-12-24 14:28:41 +0900

    debug get_chat_instances_with_highlights

--- lib/py/shared_state.py
+++ lib/py/shared_state.py
@@ -209,7 +209,7 @@ class StateManager:
                     chat_instance.chat_analyzer and
                     hasattr(chat_instance.chat_analyzer, 'highlights_dict')):
                     
-                    highlights_count = len(chat_instance.chat_analyzer.highlights_dict)
+                    highlights_count = len(*[chat_instance.chat_analyzer.highlights_dict[highlights] for highlights in chat_instance.chat_analyzer.highlights_dict])
                     if highlights_count > 0:
                         # 채널명 가져오기
                         try:
