8fc42c4641c0c839d81bbf21b311afc554f87ff9
kimboxu <kimboxu@gmail.com>
2026-01-24 21:46:32 +0900

    debug get_streamers

--- lib/py/base.py
+++ lib/py/base.py
@@ -227,7 +227,7 @@ async def DataBaseVars(init: initVar, is_start = False):
 					return {}
 
 				return {
-					platform: g.drop(columns="platform").set_index(index_col)
+					platform: g
 					for platform, g in df.groupby("platform")
 				}
 
