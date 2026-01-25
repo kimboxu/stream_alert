a5a4f7d4d38d5abd3536e7e05dcfd5205f58c36b
kimboxu <kimboxu@gmail.com>
2026-01-25 13:19:26 +0900

    debug DataBaseVars

--- lib/py/base.py
+++ lib/py/base.py
@@ -227,7 +227,7 @@ async def DataBaseVars(init: initVar, is_start = False):
 					return {}
 
 				return {
-					platform: g.set_index(index_col)
+					platform: g.set_index(index_col, drop=False)
 					for platform, g in df.groupby("platform")
 				}
 
